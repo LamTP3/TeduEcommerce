c4cf8842c6a4d3f8adb210b2a98ba9151dc085cd
Phuc Lam <phuclamtran2810@gmail.com>
2026-08-25 16:03:22 +0700

    feat: Setup CRUD API for Product categories

---
+++ aspnet-core/src/TeduEcommerce.Admin.Application.Contracts/ProductCategories/CreateUpdateProductCategoryDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduEcommerce.Admin.ProductCategories
+{
+    public class CreateUpdateProductCategoryDto
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public string Slug { get; set; }
+
+        public int SortOder { get; set; }
+
+        public string CoverPicture { get; set; }
+
+        public bool Visibility { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Guid? ParentId { get; set; }
+
+        public string SeoMetaDescription { get; set; }
+    }
+}
---
+++ aspnet-core/src/TeduEcommerce.Admin.Application.Contracts/ProductCategories/ProductCategoryDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace TeduEcommerce.Admin.ProductCategories
+{
+    public class ProductCategoryDto: IEntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public string Slug { get; set; }
+
+        public int SortOder { get; set; }
+
+        public string CoverPicture { get; set; }
+
+        public bool Visibility { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public Guid? ParentId { get; set; }
+
+        public string SeoMetaDescription { get; set; }
+        public Guid Id { get ; set ; }
+    }
+}
