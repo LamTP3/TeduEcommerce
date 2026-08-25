adf646a5256418b1bd1bdd0ce46904561743dfb5
Phuc Lam <phuclamtran2810@gmail.com>
2026-08-25 14:01:43 +0700

    feat: show profile and logout button

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { AuthService } from './shared/services/auth.service';
+import { Router } from '@angular/router';
+import { LOGIN_URL } from './shared/constants/url.const';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <router-outlet></router-outlet>
-  `,
+  template: ` <router-outlet></router-outlet> `,
 })
 export class AppComponent {
-    menuMode = 'static';
+  menuMode = 'static';
 
-    constructor(private primengConfig: PrimeNGConfig) { }
+  constructor(
+    private primengConfig: PrimeNGConfig,
+    private authService: AuthService,
+    private router: Router,
+  ) {}
 
-    ngOnInit() {
-        this.primengConfig.ripple = true;
-        document.documentElement.style.fontSize = '14px';
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    document.documentElement.style.fontSize = '14px';
+    if (this.authService.isAuthenticated() == false) {
+      this.router.navigate([LOGIN_URL]);
     }
+  }
 }
--- angular/src/app/shared/services/auth.service.ts
+++ angular/src/app/shared/services/auth.service.ts
@@ -4,6 +4,7 @@ import { LoginResponseDto } from '../models/login-response.dto';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants/keys.const';
 
 @Injectable({
   providedIn: 'root',
@@ -27,5 +28,14 @@ export class AuthService {
       data,
       { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } },
     );
+  }
+
+  public isAuthenticated(): boolean {
+    return localStorage.getItem(ACCESS_TOKEN) != null;
+  }
+
+  public logout() {
+    localStorage.removeItem(ACCESS_TOKEN);
+    localStorage.removeItem(REFRESH_TOKEN);
   }
 }
