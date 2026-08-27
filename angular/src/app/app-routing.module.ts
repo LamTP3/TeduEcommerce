6928b6f6d61568af30c786ab85be4479e93f1ce6
Phuc Lam <phuclamtran2810@gmail.com>
2026-08-27 17:03:45 +0700

    feat: Build product list page UI

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -7,12 +7,18 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
-    component: AppLayoutComponent
+    component: AppLayoutComponent,
   },
-    {
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then(m => m.ProductModule),
+    component: AppLayoutComponent,
+  },
+  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
   },
+
   // {
   //   path: 'account',
   //   loadChildren: () => import('@abp/ng.account').then(m => m.AccountModule.forLazy()),
